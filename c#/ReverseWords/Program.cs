e9322e63cf9c88ce6fa93ec2e7f4558ea8347fa4
Vítek Zach <vitekzach@users.noreply.github.com>
2023-09-22 10:31:50 +0200

    ReverseWords challenge

---
+++ c#/ReverseWords/Program.cs
@@ -0,0 +1,25 @@
+﻿// https://www.codewars.com/kata/5259b20d6021e9e14c0010d4
+
+
+
+string ReverseWords(string str)
+{
+    // This will create an empty string if there are multiple spaces
+    string[] words = str.Split(" ");
+    var reversedWords = new List<string>();
+
+    foreach (string word in words)
+    {
+        char[] wordChars = word.ToCharArray();
+        Array.Reverse(wordChars);
+        reversedWords.Add(new String(wordChars));
+    }
+
+    str = String.Join(' ', reversedWords);
+    return str;
+}
+
+string toReverse = "One two three  found";
+string reversed = ReverseWords(toReverse);
+Console.WriteLine($"Original: {toReverse}");
+Console.WriteLine($"Reversed: {reversed}");
